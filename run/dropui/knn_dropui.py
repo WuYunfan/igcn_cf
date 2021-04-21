6e75c4f2797f5d49a478d7fc66b8fcc05954d62e
wuyunfan <wuyunfan15@mails.ucas.ac.cn>
2021-04-21 20:11:06 +0800

    update some indexes

--- run/dropui/knn_dropui.py
+++ run/dropui/knn_dropui.py
@@ -16,7 +16,7 @@ def main():
 
     device = torch.device('cuda')
     config = get_gowalla_config(device)
-    dataset_config, model_config, trainer_config = config[2]
+    dataset_config, model_config, trainer_config = config[3]
     dataset_config['path'] = 'data/LGCN/gowalla_ui_0_8'
 
     dataset = get_dataset(dataset_config)

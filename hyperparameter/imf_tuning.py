ba67c51f98919038fb9cde5e0fa733e4a930c2a9
wuyunfan <wuyunfan15@mails.ucas.ac.cn>
2021-09-06 16:40:34 +0800

    fix bugs

--- hyperparameter/imf_tuning.py
+++ hyperparameter/imf_tuning.py
@@ -32,7 +32,7 @@ def main():
     max_ndcg = -np.inf
     best_params = None
     for params in grid:
-        ndcg = fitness(params['lr'], params['l2_reg'], params['dropout'])
+        ndcg = fitness(params['lr'], params['l2_reg'], params['dropout'], params['aux_reg'])
         print('NDCG: {:.3f}, Parameters: {:s}'.format(ndcg, str(params)))
         if ndcg > max_ndcg:
             max_ndcg = ndcg
